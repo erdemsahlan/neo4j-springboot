5fdd415c4b9ff04f9c4a78b4b3e790e0bef45719
Erdem Şahlan <sahlanerdem@gmail.com>
2025-01-22 21:12:44 +0300

    Transaction ve multi thread testi

--- src/main/java/com/example/neo4j/Neo4jApplication.java
+++ src/main/java/com/example/neo4j/Neo4jApplication.java
@@ -2,11 +2,14 @@ package com.example.neo4j;
 import com.example.neo4j.Repository.IMovieRepository;
 import com.example.neo4j.entity.Movie;
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @EnableNeo4jRepositories(considerNestedRepositories = true)
@@ -34,13 +37,76 @@ public class Neo4jApplication  implements CommandLineRunner {
 //        }catch (Exception e){
 //            throw e;
 //        }
-        try{
-            Movie movie = movieRepository.titleQuery("The Matrix");
-            System.out.println(movie);
-        }catch (Exception e){
-            throw e;
-        }
+//        try{
+//            Movie movie = movieRepository.titleQuery("The Matrix");
+//            System.out.println(movie);
+//        }catch (Exception e){
+//            throw e;
+//        }
 
+        Thread t1 = new Thread(()->{
+            func1();
+        });
+        Thread t2 = new Thread(()->{
+            func2();
+        });
+        Thread t3 = new Thread(()->{
+            func3();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+
+    }
+
+    @Transactional
+    public void func1(){
+        for(var i = 50000; i < 55000; i++){
+            String query = "merge (m:Movie{custom:"+i+"})" +
+                    "merge(n:Movie{custom:"+(i+1)+"})" +
+                    "merge(m)-[r:bond]->(n)";
+            try (Session session = driver.session()) {
+                session.run(query);
+                System.err.println("FUNC1-->" + i);
+            }catch (Exception e){
+                throw e;
+            }
+        }
+        System.err.println("FUNC1-->END");
+    }
+
+    @Transactional
+    public void func2(){
+        for(var i = 55000; i < 60000; i++){
+            String query = "merge (m:Movie{custom:"+i+"})" +
+                    "merge(n:Movie{custom:"+(i+1)+"})" +
+                    "merge(m)-[r:bond]->(n)";
+            try (Session session = driver.session()) {
+                session.run(query);
+                System.err.println("FUNC2-->" + i);
+            }catch (Exception e){
+                throw e;
+            }
+        }
+        System.err.println("FUNC2-->END");
+    }
+    @Transactional
+    public void func3(){
+        for(var i = 65000; i < 65000; i++){
+            String query = "merge (m:Person{custom:"+i+"})" +
+                    "merge(n:Sahis{custom:"+(i+1)+"})" +
+                    "merge(m)-[r:bond]->(n)";
+            try (Session session = driver.session()) {
+                session.run(query);
+                System.err.println("FUNC3-->" + i);
+            }catch (Exception e){
+                throw e;
+            }
+        }
+        System.err.println("FUNC3-->END");
     }
 
 }
--- src/main/java/com/example/neo4j/controller/MainController.java
+++ src/main/java/com/example/neo4j/controller/MainController.java
@@ -26,4 +26,6 @@ public class MainController {
             throw e;
         }
     }
+
+
 }
